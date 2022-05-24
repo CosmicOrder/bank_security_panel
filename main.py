254a0970a96a6e2bee491592d16057de6750eae5
CosmicOrder <Matrixxturn@yandex.ru>
2022-05-25 02:03:04 +0300

    step_4: make list of active passcards using the for-loop

--- main.py
+++ main.py
@@ -8,10 +8,9 @@ django.setup()
 from datacenter.models import Passcard  # noqa: E402
 
 if __name__ == '__main__':
+    active_passcards = []
     passcards = Passcard.objects.all()
-    passcard = passcards[99]
-
-    print('owner_name:', passcard.owner_name)
-    print('passcode:', passcard.passcode)
-    print('created_at:', passcard.created_at)
-    print('is_active:', passcard.is_active)
+    for passcard in passcards:
+        if passcard.is_active:
+            active_passcards.append(passcard)
+    print(len(active_passcards))
