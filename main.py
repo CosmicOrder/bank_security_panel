9ce1a32e520df7dad29d57cbb7903de31eed50fc
CosmicOrder <Matrixxturn@yandex.ru>
2022-05-25 03:03:41 +0300

    step_8: receive all visitors

--- main.py
+++ main.py
@@ -5,8 +5,10 @@ import django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
 django.setup()
 
-from datacenter.models import Passcard  # noqa: E402
+from datacenter.models import Visit
 
 if __name__ == '__main__':
-    active_passcards = Passcard.objects.filter(is_active=True)
-    print(len(active_passcards))
+    visitors = Visit.objects.all()
+    print(visitors)
+
+
