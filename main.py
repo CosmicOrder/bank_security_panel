a9967cfe13a0f3c4e8f203f11daa4d645458bb68
CosmicOrder <Matrixxturn@yandex.ru>
2022-05-27 03:50:17 +0300

    step_10: visitors timer

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import datetime
 import os
 
 import django
+from django.utils.timezone import localtime, make_aware
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
 django.setup()
@@ -8,5 +10,10 @@ django.setup()
 from datacenter.models import Visit
 
 if __name__ == '__main__':
-    visitors = Visit.objects.filter(leaved_at=None)
-    print(visitors)
+    visitors = Visit.objects.filter()
+    for visitor in visitors:
+        entered_at = localtime(visitor.entered_at)
+        now = make_aware(datetime.datetime.now())
+        located_time = now - entered_at
+        print('Зашёл в хранилище, время по Москве: ', entered_at)
+        print('Находится в хранилище: ', located_time)
