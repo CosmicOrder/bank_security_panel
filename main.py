7bf7a6a1f0960cfd3b1f5f377be00b23594eeac0
CosmicOrder <Matrixxturn@yandex.ru>
2022-05-28 00:29:41 +0300

    step_13: get all visits by passcard

--- main.py
+++ main.py
@@ -7,14 +7,9 @@ from django.utils.timezone import localtime, make_aware
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
 django.setup()
 
-from datacenter.models import Visit
+from datacenter.models import Visit, Passcard
 
 if __name__ == '__main__':
-    visitors = Visit.objects.filter(leaved_at=None)
-    for visitor in visitors:
-        entered_at = localtime(visitor.entered_at)
-        now = make_aware(datetime.datetime.now())
-        located_time = now - entered_at
-        print(visitor.passcard)
-        print('Зашёл в хранилище, время по Москве: ', entered_at)
-        print('Находится в хранилище: ', located_time)
+    passcard = Passcard.objects.all()[0]
+    visits = Visit.objects.filter(passcard=passcard)
+    print(visits)
